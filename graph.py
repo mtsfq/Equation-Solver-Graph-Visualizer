d56df3856e8f2a1aa176034cc21437514baaa897
mtsfq <mohanadtaher2001@gmail.com>
2026-05-22 14:18:03 +0300

    new 3 graph.py

--- graph.py
+++ graph.py
@@ -4,7 +4,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sympy import *
+from sympy import symbols, lambdify
+
 from sympy.parsing.sympy_parser import (
     parse_expr,
     standard_transformations,
@@ -17,3 +18,64 @@ transformations = (
     standard_transformations +
     (implicit_multiplication_application,)
 )
+
+
+def plot_graph(expression):
+
+    expression = expression.replace(
+        "^",
+        "**"
+    )
+
+    expr = parse_expr(
+        expression,
+        transformations=transformations
+    )
+
+    function = lambdify(
+        x,
+        expr,
+        "numpy"
+    )
+
+    x_values = np.linspace(
+        -10,
+        10,
+        500
+    )
+
+    y_values = function(
+        x_values
+    )
+
+    plt.figure(
+        figsize=(8,6)
+    )
+
+    plt.plot(
+        x_values,
+        y_values,
+        label=expression
+    )
+
+    plt.axhline(
+        y=0
+    )
+
+    plt.axvline(
+        x=0
+    )
+
+    plt.grid(True)
+
+    plt.legend()
+
+    plt.title(
+        "Graph"
+    )
+
+    plt.xlabel("x")
+
+    plt.ylabel("y")
+
+    plt.show()
